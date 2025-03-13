35d461447420f1b7d2c2ff6cbf28b3374f29e886
karthikSunilVV <karthik121104@gmail.com>
2025-03-13 16:51:01 +0530

    changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS915\n";
+  cout << "PES2UG22CS915:karthik\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
